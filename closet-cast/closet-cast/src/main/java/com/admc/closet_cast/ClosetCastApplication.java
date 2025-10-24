69439e7d8f1dbd36f9de93552e90f38f01c8aa30
acidify04 <peh4622@naver.com>
2025-10-24 12:19:32 +0900

    spring boot 프로젝트 추가

---
+++ closet-cast/closet-cast/src/main/java/com/admc/closet_cast/ClosetCastApplication.java
@@ -0,0 +1,13 @@
+package com.admc.closet_cast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosetCastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClosetCastApplication.class, args);
+	}
+
+}
---
+++ closet-cast/closet-cast/src/test/java/com/admc/closet_cast/ClosetCastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admc.closet_cast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClosetCastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
