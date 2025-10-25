3a331b92e69d64856fc2b2f334ed2d7d4d53ed6e
acidify04 <peh4622@naver.com>
2025-10-25 20:33:44 +0900

    docs #1: password encoder 추가

---
+++ closet-cast/closet-cast/src/main/java/com/admc/closet_cast/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.admc.closet_cast.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/member/signUp",   // 회원가입
+                                "/api/member/login"     // 로그인 (만들 거라면)
+                        ).permitAll()
+                        .anyRequest().permitAll() // (테스트 단계에서는 전부 허용)
+                );
+
+        return http.build();
+    }
+}
