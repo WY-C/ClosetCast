3ce55ae80d04c07dbb12e711713cbc5f14aeb1e9
acidify04 <peh4622@naver.com>
2025-11-11 23:42:49 +0900

    docs: 스웨거 설정

--- closet-cast/closet-cast/src/main/java/com/admc/closet_cast/config/SecurityConfig.java
+++ closet-cast/closet-cast/src/main/java/com/admc/closet_cast/config/SecurityConfig.java
@@ -33,7 +33,10 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/member/signup", "/api/member/signin", "/api/recommend", "api/weather/get").permitAll()
+                        .requestMatchers("/api/member/signup", "/api/member/signin", "/api/recommend", "api/weather/get",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
